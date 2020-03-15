0bc21d8ed018e7b6cb9d71c9ce4e8eda9b58b413
hua_shuo <1970323510@qq.com>
2020-03-15 21:29:54 +0800

    concurrent based on multi-thread

---
+++ tiny-thread-src/sbuf.h
@@ -0,0 +1,26 @@
+//
+// From CS:APP
+//
+#ifndef SIMPLE_WEBSERVER_SBUF_H
+#define SIMPLE_WEBSERVER_SBUF_H
+
+#include "csapp.h"
+
+typedef struct {
+    int *buf;          /* Buffer array */
+    int n;             /* Maximum number of slots */
+    int front;         /* buf[(front+1)%n] is first item */
+    int rear;          /* buf[rear%n] is last item */
+    sem_t mutex;       /* Protects accesses to buf */
+    sem_t slots;       /* Counts available slots */
+    sem_t items;       /* Counts available items */
+} sbuf_t;
+
+void sbuf_init(sbuf_t *sp, int n);
+void sbuf_deinit(sbuf_t *sp);
+void sbuf_insert(sbuf_t *sp, int item);
+int sbuf_remove(sbuf_t *sp);
+int sbuf_empty(sbuf_t *sp);
+int sbuf_full(sbuf_t *sp);
+
+#endif //SIMPLE_WEBSERVER_SBUF_H
---
+++ tiny-thread-src/tiny.h
@@ -0,0 +1,19 @@
+//
+// Created by hs on 2020/3/14.
+//
+
+#ifndef SIMPLE_WEBSERVER_TINY_H
+#define SIMPLE_WEBSERVER_TINY_H
+
+void sigchld_handler(int signo);
+void doit(int fd);
+int read_requesthdrs(rio_t *rp, char *method);
+int parse_uri(char *uri, char *filename, char *cgiargs);
+void serve_static(int fd, char *filename, int filesize, char *method);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filename, char *cgiargs, char *method);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+void Im_rio_writen(int fd, void *usrbuf, size_t n); // rio_written for EPIPE error
+
+
+#endif //SIMPLE_WEBSERVER_TINY_H
